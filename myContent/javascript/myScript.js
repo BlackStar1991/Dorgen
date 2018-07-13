cc1f9c3054485c204be499bb5950d91167722f5f
Zirka Andry <a.zirka@cleack.com>
2018-07-13 18:59:59 +0300

    create progect

---
+++ myContent/javascript/myScript.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    ////// LikeLinks buttons
+    function likeLink(btn) {
+        btn.on("click", function () {
+            var urlLink = $(this).attr("data-href");
+            if (urlLink === "") {
+                alert("URL address isn't correct / шляпа");
+            } else {
+                window.open(urlLink);
+            }
+        })
+    }
+
+    likeLink($(".otziv"));
+}
